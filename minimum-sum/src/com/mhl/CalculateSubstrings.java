9300682af3fe40b30dd9e69a139c09137f7a557d
Minghui Liu <minghui.liu@optus.com.au>
2018-08-17 19:59:59 +1000

    Merged methods

--- minimum-sum/src/com/mhl/CalculateSubstrings.java
+++ minimum-sum/src/com/mhl/CalculateSubstrings.java
@@ -2,7 +2,6 @@ package com.mhl;
 
 public class CalculateSubstrings {
 
-
   public static void main(String[] args) {
     System.out.println("---------------- Result: " + calculateSubstrings("bbb"));
     System.out.println("---------------- Result: " +calculateSubstrings("baaaaa"));
@@ -18,31 +17,18 @@ public class CalculateSubstrings {
 
   public static int calculateSubstrings(String x) {
     int length = x.length();
-    System.out.println("length:" + length);
     int match = 0;
 
     for (int w = 1 ; w <= length - 1; w++) {
-      System.out.println("--------");
-      System.out.println("w:" + w);
       if ((length - w) % 2 == 0) {
         int y = (length - w)/2;
-        System.out.println("y:" + y);
-        System.out.println("WYY - Y1 " + x.substring(w, w+y));
-        System.out.println("WYY - Y2 " + x.substring(w + y));
-        System.out.println("YWY - Y1 " + x.substring(0, y));
-        System.out.println("YWY - Y2 " + x.substring(w + y));
-        System.out.println("YYW - Y2 " + x.substring(y, y + y));
-
         if (x.substring(w, w+y).equalsIgnoreCase(x.substring(w + y))) {
-          System.out.println("One!");
           match++;
         }
         if (x.substring(0, y).equalsIgnoreCase(x.substring(w + y))) {
-          System.out.println("two!");
           match++;
         }
         if (x.substring(0, y).equalsIgnoreCase(x.substring(y, y + y))){
-          System.out.println("three!");
           match++;
         }
       }
