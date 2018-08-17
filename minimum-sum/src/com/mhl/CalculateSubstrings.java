554d541f82d91ca2258ce0d7295b8fad6fcb339b
Minghui Liu <minghui.liu@optus.com.au>
2018-08-17 21:29:42 +1000

    Merged methods

--- minimum-sum/src/com/mhl/CalculateSubstrings.java
+++ minimum-sum/src/com/mhl/CalculateSubstrings.java
@@ -3,38 +3,34 @@ package com.mhl;
 public class CalculateSubstrings {
 
   public static void main(String[] args) {
-    System.out.println("---------------- Result: " + calculateSubstrings("bbb"));
-    System.out.println("---------------- Result: " +calculateSubstrings("baaaaa"));
-    System.out.println("---------------- Result: " +calculateSubstrings("abcabc"));
-    System.out.println("---------------- Result: " +calculateSubstrings("testtesttest"));
+    System.out.println("Result: " + calculateSubstrings("a"));
+    System.out.println("Result: " + calculateSubstrings("bbb"));
+    System.out.println("Result: " + calculateSubstrings("aaaaaaaaaaaa"));
+    System.out.println("Result: " + calculateSubstrings("baaaaa"));
+    System.out.println("Result: " + calculateSubstrings("abcabc"));
+    System.out.println("Result: " + calculateSubstrings("testtesttest"));
+    System.out.println("Result: " + calculateSubstrings("abcabcabc"));
+    System.out.println("Result: " + calculateSubstrings("abaabaabaabaabaabaabaabaabaabaabaabaabaabaabaabaabaabaabaabaabaabaabaaba"));
   }
 
   /**
-   * 	as WYY,YWY or	YYW
-   * @param x
-   * @return
+   * WYY,YWY or	YYW
    */
-
   public static int calculateSubstrings(String x) {
     int length = x.length();
     int match = 0;
-
-    for (int w = 1 ; w <= length - 1; w++) {
-      if ((length - w) % 2 == 0) {
-        int y = (length - w)/2;
-        if (x.substring(w, w+y).equalsIgnoreCase(x.substring(w + y))) {
+    for (int y = 1; y <= (length - 1)/2; y++) {
+        int w = length - 2 * y;
+        if (x.substring(w, w + y).equals(x.substring(w + y))) {
           match++;
         }
-        if (x.substring(0, y).equalsIgnoreCase(x.substring(w + y))) {
+        if (x.substring(0, y).equals(x.substring(w + y))) {
           match++;
         }
-        if (x.substring(0, y).equalsIgnoreCase(x.substring(y, y + y))){
+        if (x.substring(0, y).equals(x.substring(y, y + y))){
           match++;
         }
       }
-    }
-    return match;
+      return match;
   }
-
-
 }
